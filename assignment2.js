4b04bcc4f2def816e3d9e0b46a1affc97e5249bb
Apoorva <40380945+Apoorvashirikanth@users.noreply.github.com>
2024-01-11 07:55:21 +0530

    assignment2

--- assignment2.js
+++ assignment2.js
@@ -1,29 +1,36 @@
 function asciiConversions(s) {
     let result = '';
-    let modifiedAscii = [];
+    let asciiarr = [];
+    let modifiedset = new Set()
 
     for (let i = 0; i < s.length; i++) {
-        let asciiValue = s.charCodeAt(i);
+        asciiarr.push(s.charCodeAt(i));
+    }
 
-        if (asciiValue % 2 === 0 && i < s.length - 1) {
-            let newAscii = s.charCodeAt(i + 1) + (asciiValue % 7);
+    for (let i = 0; i < asciiarr.length; i++) {
+        let asciiValue = asciiarr[i]
+
+        if (asciiValue % 2 === 0 && i < s.length - 1 && !modifiedset.has(i+1)) {
+            let newAscii = asciiarr[i+1] + (asciiValue % 7);
             newAscii = (newAscii < 0 || newAscii > 127) ? 83 : newAscii;
-            modifiedAscii.push(newAscii);
+            asciiarr[i+1] = newAscii;
             result += String.fromCharCode(newAscii);
+            modifiedset.add(i+1)
         } 
-        else if (asciiValue % 2 !== 0 && i > 0) {
-            let newAscii = s.charCodeAt(i - 1) - (asciiValue % 5);
+        else if (asciiValue % 2 !== 0 && i > 0 && !modifiedset.has(i-1)) {
+            let newAscii = asciiarr[i - 1] - (asciiValue % 5);
             newAscii = (newAscii < 0 || newAscii > 127) ? 83 : newAscii;
-            modifiedAscii.push(newAscii);
+            asciiarr[i - 1] = newAscii;
             result += String.fromCharCode(newAscii);
+            modifiedset.add(i-1)
         } 
         else {
-            modifiedAscii.push(asciiValue);
+            asciiarr[i] = asciiarr[i];
             result += String.fromCharCode(asciiValue);
         }
     }
 
-    console.log(modifiedAscii);
+    console.log(asciiarr);
     return result;
 }
 
